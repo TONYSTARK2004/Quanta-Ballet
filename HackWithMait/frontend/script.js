e228903045df4ca468805ff7e670b6bd2d1f6677
Ayush Kumar <ayushrajeshmishra20@gmail.com>
2023-11-22 16:08:34 +0530

    Add files via upload
    added the front end

---
+++ HackWithMait/frontend/script.js
@@ -0,0 +1,17 @@
+function castVote(party) {
+    const voteButtons = document.querySelectorAll('.vote-btn');
+    voteButtons.forEach(button => {
+      button.disabled = true;
+      button.classList.remove('voted');
+    });
+  
+    const votedButton = document.querySelector(`.party h2:contains(${party}) + .vote-btn`);
+    votedButton.disabled = false;
+    votedButton.classList.add('voted');
+  
+    const votedMessage = document.querySelector('.voted-message');
+    votedMessage.style.display = 'block';
+    votedMessage.innerHTML = `<p>Congratulations! You have voted for ${party}.</p>`;
+  }
+  
+  
